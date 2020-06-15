be843c6c9edcdecfc2a88ee646944e4e99c8938d
Duha-Saleh <duhamahmoud096@gmail.com>
2020-06-15 05:25:26 +0300

    change

---
+++ app.js
@@ -0,0 +1,100 @@
+
+var Seattle ={ 
+
+    randomValue: Math.random(),
+    minValue: 23,
+    maxValue: 65,
+    AvgValue: 6.3,
+        getCust: function(minValue,maxValue) {
+    Seattle.Cust= generateaRandomAde(minValue,maxValue);
+        }
+    };
+    
+    function generateaRandomAde(minValue,maxValue)
+    {return Math.floor(Seattle.randomValue *(maxValue -minValue+1))+minValue};
+    Seattle.getCust(23,65);
+    console.log(Seattle);
+    
+    
+
+var h1 =Document.creatElement (h1)
+h1.innerHTML=Seattle.Hours
+    
+//     var Tokyo ={ 
+
+//         randomValue: Math.random(),
+//         minValue: 23,
+//         maxValue: 65,
+//         AvgValue: 6.3,
+//             getCust: function(minValue,maxValue) {
+//              Tokyo.Cust= generateaRandomAde(minValue,maxValue);
+//             }
+//         };
+        
+//         function generateaRandomAde(minValue,maxValue)
+//         {return Math.floor(Tokyo.randomValue *(maxValue -minValue+1))+minValue};
+//         Seattle.getCust(23,65);
+//         console.log(Tokyo);
+        
+//         var Tokyo ={ 
+        
+//             randomValue: Math.random(),
+//             minValue: 23,
+//             maxValue: 65,
+//             AvgValue: 6.3,
+//                 getCust: function(minValue,maxValue) {
+//                     Tokyo.Cust= generateaRandomAde(minValue,maxValue);
+//                 }
+//             };
+            
+//             function generateaRandomAde(minValue,maxValue)
+//             {return Math.floor(Tokyo.randomValue *(maxValue -minValue+1))+minValue};
+//             Tokyo.getCust(23,65);
+//             console.log(Tokyo);
+
+            
+//     var Tokyo ={ 
+
+//         randomValue: Math.random(),
+//         minValue: 23,
+//         maxValue: 65,
+//         AvgValue: 6.3,
+//             getCust: function(minValue,maxValue) {
+//              Tokyo.Cust= generateaRandomAde(minValue,maxValue);
+//             }
+//         };
+        
+//         function generateaRandomAde(minValue,maxValue)
+//         {return Math.floor(Tokyo.randomValue *(maxValue -minValue+1))+minValue};
+//         Seattle.getCust(23,65);
+//         console.log(Tokyo);
+        
+//         var Tokyo ={ 
+        
+//             randomValue: Math.random(),
+//             minValue: 23,
+//             maxValue: 65,
+//             AvgValue: 6.3,
+//                 getCust: function(minValue,maxValue) {
+//                     Tokyo.Cust= generateaRandomAde(minValue,maxValue);
+//                 }
+//             };
+            
+//             function generateaRandomAde(minValue,maxValue)
+//             {return Math.floor(Tokyo.randomValue *(maxValue -minValue+1))+minValue};
+//             Tokyo.getCust(23,65);
+//             console.log(Tokyo);
+// // var ul =document.
+
+// // cookies.getCust(23,65);
+// // };
+// // console.log(getCust);
+
+// // getCust: function(minValue,maxValue) {
+// //     cookies.cust= generateaRandomAde(minValue,maxValue)
+// //         },  
+    
+    
+// //     cookies.getCust(23,65);
+// //     };
+// //     console.log(getCust);
